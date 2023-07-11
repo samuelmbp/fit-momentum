2b6931f65dacfb9cddacfc9f8ff6e26830f144d7
Samuel Raducan <samuel.thembp@gmail.com>
2023-07-11 16:35:49 +0100

    Create exercise card with description, target and gif as an image

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Pagination } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
-const Exercises = () => {
-  return <div>Exercises</div>;
+import { exerciseOption, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  return (
+    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
+      <Typography variant="h4" mb="46px">
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{
+          gap: { lg: "100px", xs: "50px" },
+        }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises.map((exercise, idx) => (
+          <ExerciseCard key={idx} exercise={exercise} />
+        ))}
+      </Stack>
+    </Box>
+  );
 };
 
 export default Exercises;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,8 +19,8 @@ const Home = () => {
       />
       <Exercises
         setExercises={setExercises}
+        exercises={exercises}
         bodyPart={bodyPart}
-        setBodyPart={setBodyPart}
       />
     </Box>
   );
